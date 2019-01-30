0b77c2030d2845e6baf0df47c18aa5195c858164
Chris Breaux, Nathan Nielson and Ryan Dikeos <chris@>
2019-01-30 10:34:12 -0800

    add functionality for rolling dice and storing the value to an object

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,30 @@ function Player(name) {
 }
 
 //hold function
+// Player.prototype.rollDice = function() {
+// var resultsFromRoll = Math.floor(6*Math.random())+1;
+//   // console.log(typeof resultsFromRoll);
+//   // return resultsFromRoll;
+//   //this.roundScore = resultsFromRoll;
+// }
+
 Player.prototype.holdScore = function(roundScore){
   this.totalScore += roundScore;
   this.roundScore = 0;
 }
 
-Player.prototype.getRoundScore = function(resultsFromRoll) {
-  this.roundScore += resultsFromRoll
+Player.prototype.getRoundScore = function() {
+  var resultsFromRoll = diceRoll();
+  if(resultsFromRoll > 1) {
+  this.roundScore += resultsFromRoll;
+} else {
+  this.roundScore = 0;
+}
+  console.log(resultsFromRoll);
+}
 
+var player1 = new Player("Jim")
+
+function diceRoll() {
+  return Math.floor(6*Math.random())+1;
 }
